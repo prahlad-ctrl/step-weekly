6c2265da7a0ab5cceec3fcb9c71e8cdae910a2dd
prahlad-ctrl <alaparthi.prahlad@gmail.com>
2026-04-10 17:19:10 +0530

    week_3_4_problem6

--- src/main.java
+++ src/main.java
@@ -1,23 +1,22 @@
 import java.util.*;
 class main {
-    static int linearSearch(String[] arr, String target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(target)) return i;
+    static int floor(int[] arr, int target) {
+        int res = -1;
+        for (int x : arr) {
+            if (x <= target) res = x;
         }
-        return -1;
+        return res;
     }
 
-    static int binarySearch(String[] arr, String target) {
-        int l = 0, r = arr.length - 1;
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-
-            if (arr[mid].equals(target)) return mid;
-            else if (arr[mid].compareTo(target) < 0) l = mid + 1;
-            else r = mid - 1;
+    static int ceiling(int[] arr, int target) {
+        int res = -1;
+        for (int x : arr) {
+            if (x >= target) {
+                res = x;
+                break;
+            }
         }
-        return -1;
+        return res;
     }
 
 }
