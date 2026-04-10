ae01c08c44c38c03a7ce32a92bc86344bbaf868d
prahlad-ctrl <alaparthi.prahlad@gmail.com>
2026-04-10 17:17:54 +0530

    week_3_4_problem5

--- src/main.java
+++ src/main.java
@@ -1,43 +1,23 @@
 import java.util.*;
 class main {
-    static class Asset {
-        String name;
-        double returnRate;
-        double volatility;
-
-        Asset(String name, double r, double v) {
-            this.name = name;
-            this.returnRate = r;
-            this.volatility = v;
+    static int linearSearch(String[] arr, String target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(target)) return i;
         }
+        return -1;
     }
 
-    // Merge Sort for Assets
-    static void mergeSortAssets(Asset[] arr, int l, int r) {
-        if (l >= r) return;
+    static int binarySearch(String[] arr, String target) {
+        int l = 0, r = arr.length - 1;
 
-        int m = (l + r) / 2;
-        mergeSortAssets(arr, l, m);
-        mergeSortAssets(arr, m + 1, r);
-        mergeAssets(arr, l, m, r);
-    }
+        while (l <= r) {
+            int mid = (l + r) / 2;
 
-    static void mergeAssets(Asset[] arr, int l, int m, int r) {
-        Asset[] temp = new Asset[r - l + 1];
-        int i = l, j = m + 1, k = 0;
-
-        while (i <= m && j <= r) {
-            if (arr[i].returnRate <= arr[j].returnRate)
-                temp[k++] = arr[i++];
-            else
-                temp[k++] = arr[j++];
+            if (arr[mid].equals(target)) return mid;
+            else if (arr[mid].compareTo(target) < 0) l = mid + 1;
+            else r = mid - 1;
         }
-
-        while (i <= m) temp[k++] = arr[i++];
-        while (j <= r) temp[k++] = arr[j++];
-
-        for (int x = 0; x < temp.length; x++)
-            arr[l + x] = temp[x];
+        return -1;
     }
 
 }
