063cfe4e013de785eb47cdb285e6ea4077a16a84
prahlad-ctrl <alaparthi.prahlad@gmail.com>
2026-04-10 17:14:18 +0530

    week_3_4_problem2

--- src/main.java
+++ src/main.java
@@ -1,52 +1,45 @@
 import java.util.*;
 class main {
-    static class Transaction {
-        String id;
-        double fee;
-        String timestamp;
+    static class Client {
+        String name;
+        int riskScore;
+        double balance;
 
-        Transaction(String id, double fee, String timestamp) {
-            this.id = id;
-            this.fee = fee;
-            this.timestamp = timestamp;
+        Client(String name, int riskScore, double balance) {
+            this.name = name;
+            this.riskScore = riskScore;
+            this.balance = balance;
         }
     }
 
-    static void bubbleSortTransactions(List<Transaction> list) {
-        int n = list.size();
+    // Bubble Sort ASC
+    static void bubbleSortClients(Client[] arr) {
+        int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
-                if (list.get(j).fee > list.get(j + 1).fee) {
-                    Collections.swap(list, j, j + 1);
-                    swapped = true;
+                if (arr[j].riskScore > arr[j + 1].riskScore) {
+                    Client temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            if (!swapped) break;
         }
     }
 
-    static void insertionSortTransactions(List<Transaction> list) {
-        for (int i = 1; i < list.size(); i++) {
-            Transaction key = list.get(i);
+    // Insertion Sort DESC + balance
+    static void insertionSortClients(Client[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            Client key = arr[i];
             int j = i - 1;
 
             while (j >= 0 &&
-                    (list.get(j).fee > key.fee ||
-                            (list.get(j).fee == key.fee &&
-                                    list.get(j).timestamp.compareTo(key.timestamp) > 0))) {
-                list.set(j + 1, list.get(j));
+                    (arr[j].riskScore < key.riskScore ||
+                            (arr[j].riskScore == key.riskScore &&
+                                    arr[j].balance < key.balance))) {
+                arr[j + 1] = arr[j];
                 j--;
             }
-            list.set(j + 1, key);
-        }
-    }
-
-    static void flagHighFee(List<Transaction> list) {
-        for (Transaction t : list) {
-            if (t.fee > 50) {
-                System.out.println("High fee: " + t.id);
-            }
+            arr[j + 1] = key;
         }
     }
 }
