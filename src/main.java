7036082418baab55c532ee1a72678bd7a4832705
prahlad-ctrl <alaparthi.prahlad@gmail.com>
2026-04-10 17:16:50 +0530

    week_3_4_problem3

--- src/main.java
+++ src/main.java
@@ -1,64 +1,43 @@
 import java.util.*;
 class main {
-    static void mergeSort(int[] arr, int l, int r) {
-        if (l >= r) return;
+    static class Asset {
+        String name;
+        double returnRate;
+        double volatility;
 
-        int mid = (l + r) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
-        merge(arr, l, mid, r);
+        Asset(String name, double r, double v) {
+            this.name = name;
+            this.returnRate = r;
+            this.volatility = v;
+        }
     }
 
-    static void merge(int[] arr, int l, int m, int r) {
-        int[] temp = new int[r - l + 1];
+    // Merge Sort for Assets
+    static void mergeSortAssets(Asset[] arr, int l, int r) {
+        if (l >= r) return;
+
+        int m = (l + r) / 2;
+        mergeSortAssets(arr, l, m);
+        mergeSortAssets(arr, m + 1, r);
+        mergeAssets(arr, l, m, r);
+    }
+
+    static void mergeAssets(Asset[] arr, int l, int m, int r) {
+        Asset[] temp = new Asset[r - l + 1];
         int i = l, j = m + 1, k = 0;
 
         while (i <= m && j <= r) {
-            if (arr[i] <= arr[j]) temp[k++] = arr[i++];
-            else temp[k++] = arr[j++];
+            if (arr[i].returnRate <= arr[j].returnRate)
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
         }
 
         while (i <= m) temp[k++] = arr[i++];
         while (j <= r) temp[k++] = arr[j++];
 
-        for (int x = 0; x < temp.length; x++) {
+        for (int x = 0; x < temp.length; x++)
             arr[l + x] = temp[x];
-        }
-    }
-
-    // Quick Sort DESC
-    static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
-
-    static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j] > pivot) { // DESC
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-
-        return i + 1;
-    }
-
-    static int totalVolume(int[] arr) {
-        int sum = 0;
-        for (int x : arr) sum += x;
-        return sum;
     }
 
 }
